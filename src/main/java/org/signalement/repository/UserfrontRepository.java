3905e880a3b288009b0706e193202cbeccfded22
Hasina1234 <89484785+Hasina1234@users.noreply.github.com>
2022-01-19 19:09:28 +0100

    Update UserfrontRepository.java

--- src/main/java/org/signalement/repository/UserfrontRepository.java
+++ src/main/java/org/signalement/repository/UserfrontRepository.java
@@ -4,12 +4,20 @@
  * and open the template in the editor.
  */
 package org.signalement.repository;
+import java.util.List;
+import org.signalement.entities.Signalement;
 import org.signalement.entities.Userfront;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 /**
  *
  * @author Mamitiana
  */
 public interface UserfrontRepository extends JpaRepository<Userfront, Integer> {
-   
+    @Query("select u from Userfront u where u.username= :username and u.password= :password and u.region.id= :idregion")
+    public Userfront findUserfrontlogin(@Param("username") String username, @Param("password") String password,@Param("idregion") Integer idregion);
+    @Query("select u from Userfront u where u.region= :region")
+    public List<Userfront>findUserfrontloginByRegion(@Param("region") Integer region);
 }
+
