385533df45bf7bedf8764cdb6f285fbb9681e033
Hasina1234 <89484785+Hasina1234@users.noreply.github.com>
2022-01-19 19:58:11 +0100

    Update SignalementRepository.java

--- src/main/java/org/signalement/repository/SignalementRepository.java
+++ src/main/java/org/signalement/repository/SignalementRepository.java
@@ -3,7 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package org.signalement.repository;
+import java.util.Date;
 import java.util.List;
 import org.signalement.entities.Signalement;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -15,7 +21,14 @@ import org.springframework.data.repository.query.Param;
  */
 public interface SignalementRepository extends JpaRepository<Signalement, Integer> {
     public List<Signalement>  findByRegionIsNull();
+
+    @Query("select s from Signalement s where s.type.id= :type")
+    public List<Signalement>findSignalementByType(@Param("type") int type);
     
-    @Query("SELECT s FROM Signalement s WHERE s.type.id = :idtype")
-   List<Signalement>  chercherpartype(@Param("idtype") int idtype);
+    @Query("select s from Signalement s where s.daty= :daty")
+    public List<Signalement>findSignalementByDaty(@Param("daty") String daty);
+    
+    @Query("select s from Signalement s where s.statut.id= :statut")
+    public List<Signalement>findSignalementByStatut(@Param("statut") int statut);
 }
+
