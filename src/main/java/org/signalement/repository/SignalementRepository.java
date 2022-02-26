9f4162a67ca123c478a20be4b27dfdff1a168b06
MamyGhost <80816758+MamyGhost@users.noreply.github.com>
2022-02-26 22:12:07 +0300

    shhhhh

--- src/main/java/org/signalement/repository/SignalementRepository.java
+++ src/main/java/org/signalement/repository/SignalementRepository.java
@@ -3,13 +3,20 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package org.signalement.repository;
 import java.util.Date;
 import java.util.List;
 import org.signalement.entities.Signalement;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 /**
  *
  * @author Mamitiana
@@ -20,13 +27,154 @@ public interface SignalementRepository extends JpaRepository<Signalement, Intege
     @Query("select s from Signalement s where s.type.id= :type")
     public List<Signalement>findSignalementByType(@Param("type") int type);
     
+    @Query("select s from Signalement s where s.type.id= :type and s.region.id = :id")
+    public List<Signalement>findSignalementByTypeandRegion(@Param("type") int type,@Param("id") Integer id);
+
+    @Query("select s from Signalement s where s.description= :description and s.region.id = :id")
+    public List<Signalement>findSignalementByNomandRegion(@Param("description") String description,@Param("id") Integer id);
+    
+     @Query("select s from Signalement s where s.description= :description and s.region.id = :id")
+    public Signalement getSignalementByNomandRegion(@Param("description") String description,@Param("id") Integer id);
+
+
+    @Query("SELECT s FROM Signalement s WHERE s.type.id = :idtype")
+   List<Signalement>  chercherpartype(@Param("idtype") int idtype);
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
+    @Query( value = "select SUM(CASE month(s.daty)\n" +
+"            WHEN 1 THEN 1\n" +
+"            ELSE 0\n" +
+"            END) \"Janvier\",\n" +
+"            SUM(CASE month(s.daty)\n" +
+"            WHEN 2 THEN 1\n" +
+"            ELSE 0\n" +
+"            END) \"Fevrier\", \n" +
+"            SUM(CASE month(s.daty)\n" +
+"            WHEN 3 THEN 1\n" +
+"            ELSE 0\n" +
+"            END) \"Mars\", \n" +
+"            SUM(CASE month(s.daty)\n" +
+"            WHEN 4 THEN 1\n" +
+"            ELSE 0\n" +
+"            END) \"Avril\", \n" +
+"            SUM(CASE month(s.daty)\n" +
+"            WHEN 5 THEN 1\n" +
+"            ELSE 0\n" +
+"            END) \"Mai\", \n" +
+"            SUM(CASE month(s.daty)\n" +
+"            WHEN 6 THEN 1\n" +
+"            ELSE 0\n" +
+"            END) \"Juin\", \n" +
+"            SUM(CASE month(s.daty)\n" +
+"            WHEN 7 THEN 1\n" +
+"            ELSE 0\n" +
+"            END) \"Juillet\", \n" +
+"            SUM(CASE month(s.daty)\n" +
+"            WHEN 8 THEN 1\n" +
+"            ELSE 0\n" +
+"            END) \"Aout\", \n" +
+"            SUM(CASE month(s.daty)\n" +
+"            WHEN 9 THEN 1\n" +
+"            ELSE 0\n" +
+"            END) \"Septembre\", \n" +
+"            SUM(CASE month(s.daty)\n" +
+"            WHEN 10 THEN 1\n" +
+"            ELSE 0\n" +
+"            END) \"Octobre\", \n" +
+"            SUM(CASE month(s.daty)\n" +
+"            WHEN 11 THEN 1\n" +
+"            ELSE 0\n" +
+"            END) \"Novembre\", \n" +
+"            SUM(CASE month(s.daty)\n" +
+"            WHEN 12 THEN 1\n" +
+"            ELSE 0\n" +
+"            END) \"Decembre\"\n" +
+"            FROM Signalement s where year(s.daty)=year(:now)",
+            nativeQuery=true)
+            public List<Object[]>findstatmonthly(@Param("now") Date now);
+            
+            @Query(value="select\n" +
+" 	CASE\n" +
+" 		WHEN isa is null THEN 0\n" +
+" 		ELSE isa\n" +
+" 	END \"isa\"\n" +
+" from type left join (select count(id) as isa,type as t from signalement group by type) as s on id=s.t;",nativeQuery=true)
+    public List<Integer>findGroupByType();
+    
+            
+            
+    @Query(value="select\n" +
+"	region.id, \n" +
+"	region.Nom,\n" +
+"	count(Signalement.id) as nbr,\n" +
+"	sum(\n" +
+"		CASE Statut\n" +
+"			when 1 then 1\n" +
+"			else 0\n" +
+"		END\n" +
+"	) \"attente\",\n" +
+"	sum(\n" +
+"		CASE Statut\n" +
+"			when 2 then 1\n" +
+"			else 0\n" +
+"		END\n" +
+"	) \"traitement\",\n" +
+"	sum(\n" +
+"		CASE Statut\n" +
+"			when 3 then 1\n" +
+"			else 0\n" +
+"		END\n" +
+"	) \"termine\"\n" +
+"from region\n" +
+"	left join Signalement\n" +
+"	on region.id=Signalement.region\n" +
+"	group by region.id order by nbr DESC",nativeQuery=true)
+    public List<Object[]>getStatRegion();
+    
+    @Query(value="select \n" +
+"	CASE \n" +
+"		WHEN count(Signalement.id) is null then 0\n" +
+"		else count(Signalement.id)\n" +
+"	END \"nbr\"\n" +
+"from statut\n" +
+"left join\n" +
+"signalement on statut.id=Signalement.Statut\n" +
+"group by Statut.id order By Statut.id asc",nativeQuery=true)
+   public List<Integer>findGroupByStatut(); 
+    
+    
     @Query("select s from Signalement s where s.daty= :daty")
     public List<Signalement>findSignalementByDaty(@Param("daty") String daty);
     
     @Query("select s from Signalement s where s.statut.id= :statut")
     public List<Signalement>findSignalementByStatut(@Param("statut") int statut);
-<<<<<<< Updated upstream
-=======
+    
+     @Query("select s from Signalement s where s.daty= :daty and s.region.id = :id")
+    public List<Signalement>findSignalementByDatyandRegion(@Param("daty") Date daty,@Param("id") Integer id);
+    
+    @Query("select s from Signalement s where s.statut.etat= :statut and s.region.id = :id")
+    public List<Signalement>findSignalementByStatutandRegion(@Param("statut") String statut,@Param("id") Integer id);
+
+
+     @Query("select s from Signalement s where s.type.nom= :type and s.region.id = :id")
+    public List<Signalement>findSignalementByTypeandRegion(@Param("type") String type,@Param("id") Integer id);
+
+       @Query("select s from Signalement s where s.type.nom= :type and s.daty= :daty and s.statut.etat= :statut and s.region.id = :id")
+    public List<Signalement>findSignalementByRecherche(@Param("type") String type,@Param("daty") Date daty,@Param("statut") String statut,@Param("id") Integer id);
+
+    @Query(value = "select * from Signalement s order by s.daty DESC limit :inf,:sup",nativeQuery=true)
+    public List<Signalement>findWithPagination(@Param("inf") int inf,@Param("sup") int sup);
+
+
+    @Query(value = "select * from Signalement s where s.Utilisateur = :user order by s.daty DESC limit :inf,:sup",nativeQuery=true)
+    public List<Signalement>findByUserWithPagination(@Param("user") int user,@Param("inf") int inf,@Param("sup") int sup);
+
+    @Modifying
+    @Query(value = "update signalement s set s.statut.etat=3 where s.description= :description" ,nativeQuery=true)
+    public void statutUpdate(@Param("description") String description);
+
+
+
+
 
     // @Query("select s from signalement s join userfront u on s.region.id=u.region.id join tokenfront t on u.id=t.userfront.id where t.token= :token ")
     // public List<Signalement> findSignalementByRegion(@Param("token") String token);
@@ -35,5 +183,6 @@ public interface SignalementRepository extends JpaRepository<Signalement, Intege
     public List<Signalement>findSignalementByRegion(@Param("token") String token);
  
 
->>>>>>> Stashed changes
 }
+
+
--- src/main/java/org/signalement/webservice/SignalementControl.java
+++ src/main/java/org/signalement/webservice/SignalementControl.java
@@ -6,21 +6,29 @@
 package org.signalement.webservice;
 
 import java.net.URI;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import org.signalement.entities.Signalement;
+import org.signalement.entities.Statut;
 import org.signalement.repository.SignalementRepository;
+import org.signalement.repository.StatutRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  *
@@ -31,6 +39,8 @@ public class SignalementControl {
     
      @Autowired
     private SignalementRepository signalementRepository;
+      @Autowired
+    private StatutRepository statutRepository;
      
      @GetMapping("/wb/signalement/{id}")
         public ResponseEntity<Signalement> getSignalementById(@PathVariable("id") int id) {
@@ -54,7 +64,7 @@ public class SignalementControl {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Signalement non trouve");
           }
         }
-        
+                
     
      @GetMapping("/wb/statut/{statut}/signalement")
     public ResponseEntity<List<Signalement>> listSignalementParStatut(@PathVariable("statut") int statut){
@@ -65,8 +75,74 @@ public class SignalementControl {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Signalement non trouve");
           }
 }
+  
+
+  @GetMapping("/wb/userfront/signalement/type")
+        public ResponseEntity<List<Signalement>> getSignalementBytypeAndRegion(@RequestParam(name="type") String type,@RequestParam(name="idregion") int idregion) {
+          List<Signalement> sData = signalementRepository.findSignalementByTypeandRegion(type,idregion);
+
+          if (!sData.isEmpty()) {
+            return new ResponseEntity<>(sData, HttpStatus.OK);
+          } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Signalement non trouve");
+          }
+        }
+
+
+       @GetMapping("/wb/userfront/signalement/statut")
+    public ResponseEntity<List<Signalement>> listSignalementParStatutAndRegion(@RequestParam(name="statut") String statut,@RequestParam(name="idregion") int idregion){
+      List<Signalement> sData= signalementRepository.findSignalementByStatutandRegion(statut,idregion);
+    if (!sData.isEmpty()) {
+            return new ResponseEntity<>(sData, HttpStatus.OK);
+          } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Signalement non trouve");
+          }
+}
+
+
     
-    @PostMapping("/wb/signalement")
+         @GetMapping("/wb/userfront/signalement/date")          
+    public ResponseEntity<List<Signalement>> listSignalementParStatut(@RequestParam(name="daty") String daty,@RequestParam(name="idregion") int idregion) throws ParseException{
+        Date date1=new SimpleDateFormat("yyyy-MM-dd").parse(daty);
+      List<Signalement> sData= signalementRepository.findSignalementByDatyandRegion(date1,idregion);
+    if (!sData.isEmpty()) {
+            return new ResponseEntity<>(sData, HttpStatus.OK);
+          } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Signalement non trouve");
+          }
+}
+
+
+         @GetMapping("/wb/userfront/signalement/description")
+    public ResponseEntity<List<Signalement>> getSignalementByNom(@RequestParam(name="description") String description,@RequestParam(name="idregion") int idregion) throws ParseException{
+      List<Signalement> sData= signalementRepository.findSignalementByNomandRegion(description,idregion);
+    if (!sData.isEmpty()) {
+            return new ResponseEntity<>(sData, HttpStatus.OK);
+          } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Signalement non trouve");
+          }
+}
+
+     @GetMapping("/wb/userfront/signalement/description2")
+    public ResponseEntity<Signalement> getSignalementByNom2(@RequestParam(name="description") String description,@RequestParam(name="idregion") int idregion) throws ParseException{
+      Signalement sData= signalementRepository.getSignalementByNomandRegion(description,idregion);
+        
+      return new ResponseEntity<>(sData, HttpStatus.OK);
+}
+
+
+        @GetMapping("/wb/userfront/signalement/recherche")
+    public ResponseEntity<List<Signalement>> listSignalementParRecherche(@RequestParam(name="type") String type,@RequestParam(name="daty") String daty,@RequestParam(name="statut") String statut,@RequestParam(name="idregion") int idregion) throws ParseException{
+        Date date1=new SimpleDateFormat("yyyy-MM-dd").parse(daty);
+      List<Signalement> sData= signalementRepository.findSignalementByRecherche(type,date1,statut,idregion);
+    if (!sData.isEmpty()) {
+            return new ResponseEntity<>(sData, HttpStatus.OK);
+          } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Signalement non trouve");
+          }
+}
+    
+    @PostMapping("/wb/userfront/signalement")
     public ResponseEntity<Signalement> save(@RequestBody Signalement signalement){
         // inserte admin
        try {
@@ -85,7 +161,23 @@ public class SignalementControl {
     
 }
 
-@GetMapping("/wb/region/{token}/signalement")
+    @PutMapping("/wb/userfront/signalement/statut/{description}/{idregion}")
+    public ResponseEntity <Signalement> updateStatut(@PathVariable("description") String description,@PathVariable("idregion") int idregion){
+      Signalement sData= signalementRepository.getSignalementByNomandRegion(description,idregion);
+      Statut testa =statutRepository.findById(3).get();
+      
+         sData.setStatut(testa);
+      
+     
+       try{
+            signalementRepository.save(sData);
+          }catch(Exception ex){
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,"Erreur du serveur:"+ex.getMessage());
+          }
+     return new ResponseEntity<>(sData, HttpStatus.OK);
+}
+
+@GetMapping("/wb/userfront/region/{token}/signalement")
         public ResponseEntity<List<Signalement>> getSignalementByRegion(@PathVariable("token") String token) {
           
           List<Signalement> sData = signalementRepository.findSignalementByRegion(token);
@@ -98,3 +190,4 @@ public class SignalementControl {
         }
     
 }
+
